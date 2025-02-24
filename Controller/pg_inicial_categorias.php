1b719cb4344289f87ca382c6494fad72cd8beaf2
isa-miho <isabella.miho@gmail.com>
2025-02-24 17:12:22 -0400

    Melhora dos cards

---
+++ Controller/pg_inicial_categorias.php
@@ -0,0 +1,52 @@
+<?php
+include 'menu-pg-inicial.html';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial Categorias</title>
+    <link rel="stylesheet" href="../View/CSS/pg_inicial_categorias.css">
+</head>
+<body>
+    <!-- Inicio das categorias -->
+    <section id="pg_inicial_categorias">
+        <h3 class="categoria_title_pg_inicial">Categorias</h3>
+        <div class="categoria_content_pg_inicial">
+
+            <div class="categoria_cards_pg_inicial card-galinha-pg_inicial_categorias">
+                <a href="https://chromewebstore.google.com/" class="card_categoria_pg_inicial">
+                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/galinha_categorias.svg" alt="">
+                </a>
+            </div>
+
+            <div class="categoria_cards_pg_inicial card-boi-pg_inicial_categorias">
+                <a href="" class="card_categoria_pg_inicial">
+                    <img  src="../View/Public/Imagens/img_pg_inicial_categorias/boi_categoria_pg_inicial.svg" alt="">
+                </a>
+            </div>
+
+            <div class="categoria_cards_pg_inicial">
+                <a href="" class="card_categoria_pg_inicial card-cavalo-pg_inicial_categorias">
+                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/cavalo_categorias_pg_inicial.svg" alt="">
+                </a>
+            </div>
+
+            <div class="categoria_cards_pg_inicial">
+                <a href="" class="card_categoria_pg_inicia card-produtos-pg_inicial_categoriasl">
+                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/produtos_categorias_pg_inicial.svg" alt="">
+                </a>
+            </div>
+
+            <div class="categoria_cards_pg_inicial">
+                <a href="" class="card_categoria_pg_inicial card-campeao-pg_inicial_categorias">
+                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/campeoes_categorias_pg_inicial.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
