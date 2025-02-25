3ecf96763465ce0ed8b304187691dc710e14b613
annax08 <annalaurah768@gmail.com>
2025-02-25 17:04:42 -0400

    arrumando classes

---
+++ Controller/detalhesProduto.php
@@ -0,0 +1,48 @@
+<?php
+include 'menu-pg-inicial.html'
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galo Índio de León - John Rooster</title>
+    <link rel="stylesheet" href="detalhesProduto.html">
+    <link rel="stylesheet" href="../View/Public/CSS/detalhesdoproduto.css">
+</head>
+<body class="body-detalhes-produtos">
+    <main class="main-detalhes-produtos">
+        <section class="produto-detalhes-produtos">
+            <div class="imagens-detalhes-produto">
+                <img src="galo-principal.jpg" alt="Galo Índio de León" class="imagem-grande-detalhes-produto">
+                <div class="miniaturas-detalhes-produto">
+                    <img src="../View/Public/Imagens/Barred_Plymouth_Rock_Rooster_001.jpg" alt="Galo lateral">
+                    <img src="../" alt="Galo de perfil">
+                    <img src="galo3.jpg" alt="Galo em pé">
+                </div>
+            </div> 
+
+            <div class="info-produto-detalhes-produto">  
+                <h2 class="titulo-produto--detalhes-produto">Galo Índio de León</h2> 
+                <div class="estrelas-detalhes-produto">
+                    ★★★★★ <span>4.9 (204)</span>
+                </div>
+                <p class="preco-detalhes-produto">R$ 5.000,00</p>  
+                <button class="botao-carrinho-detalhes-produto">Adicionar ao carrinho</button>
+                <button class="botao-comprar-detalhes-produto">Comprar</button>
+
+                <div class="descricao-detalhes-produto">
+                    <h3>Informações</h3>
+                    <p><strong>Peso:</strong> 5kg</p>
+                    <p><strong>Idade:</strong> 1 ano</p>
+                    <p><strong>Raça:</strong> Criado a partir dos melhores exemplares, o Galo Índio de León possui uma linhagem seletiva que garante não apenas beleza, mas também vigor e adaptabilidade.</p>
+                    <p><strong>Criação:</strong> Os galos devem ter um ambiente amplo, limpo e bem ventilado, com áreas cobertas para proteção contra intempéries e exercícios.</p>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
+
+
