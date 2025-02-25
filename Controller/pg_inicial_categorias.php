107a1a9da4edb6867af0a1ae481afda11cc3c4bb
isa-miho <isabella.miho@gmail.com>
2025-02-25 17:04:22 -0400

    Melhora dos cards

--- Controller/pg_inicial_categorias.php
+++ Controller/pg_inicial_categorias.php
@@ -14,35 +14,30 @@ include 'menu-pg-inicial.html';
     <!-- Inicio das categorias -->
     <section id="pg_inicial_categorias">
         <h3 class="categoria_title_pg_inicial">Categorias</h3>
-        <div class="categoria_content_pg_inicial">
+        <div class="categoria_area_cards_pg_inicial">
 
-            <div class="categoria_cards_pg_inicial card-galinha-pg_inicial_categorias">
-                <a href="https://chromewebstore.google.com/" class="card_categoria_pg_inicial">
-                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/galinha_categorias.svg" alt="">
+            <div class="categoria_cards_pg_inicial ">
+                <a href="https://chromewebstore.google.com/" class="card_categoria_pg_inicial card-galinha-pg_inicial_categorias">
                 </a>
             </div>
 
-            <div class="categoria_cards_pg_inicial card-boi-pg_inicial_categorias">
-                <a href="" class="card_categoria_pg_inicial">
-                    <img  src="../View/Public/Imagens/img_pg_inicial_categorias/boi_categoria_pg_inicial.svg" alt="">
+            <div class="categoria_cards_pg_inicial ">
+                <a href="" class="card_categoria_pg_inicial card-boi-pg_inicial_categorias">
                 </a>
             </div>
 
             <div class="categoria_cards_pg_inicial">
                 <a href="" class="card_categoria_pg_inicial card-cavalo-pg_inicial_categorias">
-                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/cavalo_categorias_pg_inicial.svg" alt="">
                 </a>
             </div>
 
             <div class="categoria_cards_pg_inicial">
-                <a href="" class="card_categoria_pg_inicia card-produtos-pg_inicial_categoriasl">
-                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/produtos_categorias_pg_inicial.svg" alt="">
+                <a href="" class="card_categoria_pg_inicial card-produtos-pg_inicial_categoriasl">
                 </a>
             </div>
 
             <div class="categoria_cards_pg_inicial">
                 <a href="" class="card_categoria_pg_inicial card-campeao-pg_inicial_categorias">
-                    <img src="../View/Public/Imagens/img_pg_inicial_categorias/campeoes_categorias_pg_inicial.svg" alt="">
                 </a>
             </div>
         </div>
