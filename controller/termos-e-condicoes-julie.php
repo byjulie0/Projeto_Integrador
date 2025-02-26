b5053970949b6be19013f9414d7a188431401785
byjulie0 <julievieiramartins0@gmail.com>
2025-02-26 14:43:31 -0400

    Estilizando a tela de termos e condições

--- controller/termos-e-condicoes-julie.php
+++ controller/termos-e-condicoes-julie.php
@@ -9,73 +9,75 @@ include 'menu-pg-inicial.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Termos e Condições</title> 
     <link rel="stylesheet" href=".././view/public/css/termos-e-condicoes.css">
-    <link rel="stylesheet" href="/view/public/css/styles.css">
+    <link rel="stylesheet" href="../view/public/css/styles.css">
 </head>
 <body>
     <header>
         <div class="titulo-termo-e-condicoes">
             <h1 >Termos e Condições</h1>
         </div>
-        <h2>Bem-vindo(a) à John Rooster! Ao utilizar nossos serviços e realizar compras em nossa plataforma online, você concorda com os seguintes Termos e Condições. Por favor, leia atentamente antes de prosseguir com qualquer transação.
 
-<span>1. Sobre a Empresa</span>
-A John Rooster é uma empresa especializada na venda de animais bovinos, galináceos e equinos, além de produtos relacionados ao meio agropecuário. Todas as vendas são realizadas exclusivamente online, e os pagamentos são processados via WhatsApp.
+        <div class="texto-termos-e-econdicoes">
+            <h2>Bem-vindo(a) à John Rooster! Ao utilizar nossos serviços e realizar compras em nossa plataforma online, você concorda com os seguintes Termos e Condições. Por favor, leia atentamente antes de prosseguir com qualquer transação.
 
-<span>2. Condições de Venda</span>
-2.1. <span>Produtos e Animais</span>
+            <span>1. Sobre a Empresa</span>
+            A John Rooster é uma empresa especializada na venda de animais bovinos, galináceos e equinos, além de produtos relacionados ao meio agropecuário. Todas as vendas são realizadas exclusivamente online, e os pagamentos são processados via WhatsApp.
 
-A John Rooster se compromete a fornecer informações claras e precisas sobre os animais e produtos disponíveis para venda, incluindo espécie, raça, idade, saúde e características relevantes.
+            <span>2. Condições de Venda</span>
+            2.1. <span>Produtos e Animais</span>
 
-As imagens e descrições dos produtos são meramente ilustrativas. Pequenas variações podem ocorrer.
+            A John Rooster se compromete a fornecer informações claras e precisas sobre os animais e produtos disponíveis para venda, incluindo espécie, raça, idade, saúde e características relevantes.
 
-2.2. <span>Pagamento</span>
+            As imagens e descrições dos produtos são meramente ilustrativas. Pequenas variações podem ocorrer.
 
-O pagamento será realizado exclusivamente via WhatsApp, mediante envio de comprovante de transferência ou outro método acordado com a equipe de atendimento.
+            2.2. <span>Pagamento</span>
 
-A confirmação do pagamento é necessária para a finalização da compra.
+            O pagamento será realizado exclusivamente via WhatsApp, mediante envio de comprovante de transferência ou outro método acordado com a equipe de atendimento.
 
-2.3. <span>Entrega</span>
-A John Rooster realiza entregas dos animais e produtos adquiridos por meio de sua frota própria de caminhões, garantindo maior segurança e controle durante o transporte. O valor do frete já está incluso no preço final do produto ou animal comprado, proporcionando praticidade e transparência ao cliente.
+            A confirmação do pagamento é necessária para a finalização da compra.
 
-No entanto, é importante observar as seguintes condições:
+            2.3. <span>Entrega</span>
+            A John Rooster realiza entregas dos animais e produtos adquiridos por meio de sua frota própria de caminhões, garantindo maior segurança e controle durante o transporte. O valor do frete já está incluso no preço final do produto ou animal comprado, proporcionando praticidade e transparência ao cliente.
 
-A John Rooster se responsabiliza pelo transporte até o local combinado com o comprador, desde que esteja dentro da área de cobertura atendida pela empresa.
+            No entanto, é importante observar as seguintes condições:
 
-O comprador deve fornecer um endereço de entrega adequado e acessível para veículos de grande porte, além de estar disponível no horário agendado para recebimento.
+            A John Rooster se responsabiliza pelo transporte até o local combinado com o comprador, desde que esteja dentro da área de cobertura atendida pela empresa.
 
-Em caso de imprevistos durante o transporte, como condições climáticas adversas ou problemas mecânicos, a John Rooster entrará em contato com o comprador para reprogramar a entrega.
+            O comprador deve fornecer um endereço de entrega adequado e acessível para veículos de grande porte, além de estar disponível no horário agendado para recebimento.
 
-Caso o comprador prefira retirar o produto ou animal diretamente em nosso estabelecimento, o valor do frete incluso no preço será desconsiderado, e um desconto equivalente poderá ser aplicado.
+            Em caso de imprevistos durante o transporte, como condições climáticas adversas ou problemas mecânicos, a John Rooster entrará em contato com o comprador para reprogramar a entrega.
 
-<span>3. Responsabilidades do Comprador</span>
-3.1. <span>Verificação de Informações</span>
+            Caso o comprador prefira retirar o produto ou animal diretamente em nosso estabelecimento, o valor do frete incluso no preço será desconsiderado, e um desconto equivalente poderá ser aplicado.
 
-Cabe ao comprador verificar as informações dos animais ou produtos antes da compra, incluindo condições de saúde, documentação e adequação às suas necessidades.
+            <span>3. Responsabilidades do Comprador</span>
+            3.1. <span>Verificação de Informações</span>
 
-3.2. <span>Cuidados com os Animais</span>
+            Cabe ao comprador verificar as informações dos animais ou produtos antes da compra, incluindo condições de saúde, documentação e adequação às suas necessidades.
 
-O comprador assume total responsabilidade pelos cuidados com os animais após a conclusão da venda, incluindo alimentação, saúde e bem-estar.
+            3.2. <span>Cuidados com os Animais</span>
 
-<span>5. Privacidade e Segurança</span>
-5.1.<span> Dados Pessoais</span>
+            O comprador assume total responsabilidade pelos cuidados com os animais após a conclusão da venda, incluindo alimentação, saúde e bem-estar.
 
-A John Rooster se compromete a proteger os dados pessoais fornecidos pelos clientes, utilizando-os apenas para finalidades relacionadas à venda e ao atendimento.
+            <span>5. Privacidade e Segurança</span>
+            5.1.<span> Dados Pessoais</span>
 
-5.2. <span>Segurança no Pagamento</span>
+            A John Rooster se compromete a proteger os dados pessoais fornecidos pelos clientes, utilizando-os apenas para finalidades relacionadas à venda e ao atendimento.
 
-Recomendamos que os clientes evitem compartilhar informações sensíveis no WhatsApp, como números de cartão de crédito. Utilize métodos seguros, como transferência bancária.
+            5.2. <span>Segurança no Pagamento</span>
 
-<span>6. Alterações nos Termos e Condições</span>
-A John Rooster reserva-se o direito de alterar estes Termos e Condições a qualquer momento. As mudanças serão comunicadas aos clientes por meio de nossos canais oficiais.
+            Recomendamos que os clientes evitem compartilhar informações sensíveis no WhatsApp, como números de cartão de crédito. Utilize métodos seguros, como transferência bancária.
 
-<span>7. Contato</span>
-Para dúvidas, reclamações ou sugestões, entre em contato conosco através do WhatsApp: [inserir número de telefone] ou e-mail: [inserir e-mail].
+            <span>6. Alterações nos Termos e Condições</span>
+            A John Rooster reserva-se o direito de alterar estes Termos e Condições a qualquer momento. As mudanças serão comunicadas aos clientes por meio de nossos canais oficiais.
 
-Agradecemos por escolher a John Rooster! Estamos à disposição para oferecer os melhores produtos e serviços para o seu negócio agropecuário.
+            <span>7. Contato</span>
+            Para dúvidas, reclamações ou sugestões, entre em contato conosco através do WhatsApp: [inserir número de telefone] ou e-mail: [inserir e-mail].
 
-John Rooster
-Sua parceira em soluções agropecuárias.
-</h2>
+            Agradecemos por escolher a John Rooster! Estamos à disposição para oferecer os melhores produtos e serviços para o seu negócio agropecuário.
+
+            John Rooster
+            Sua parceira em soluções agropecuárias.</h2>
+        </div>
     </header>    
 </body>
 </html>
