a322354782db460a7a39b70ed58b0fd8767320d1
mariaaa <toledomiranda.maria23@gmail.com>
2025-03-07 15:18:45 -0400

    arrumando conflitos

--- controller/PHP/formulario_cadastro.php
+++ controller/PHP/formulario_cadastro.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>John Rooster - Cadastro</title>
     <link rel="stylesheet" href="../../view/public/css/formulario_cadastro.css">
+    <link rel="stylesheet" href="../view/public/css/cliente.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
 </head>
@@ -47,6 +48,4 @@
 </body>
 </html>
 
-<?php
-    include 'footer_cliente.php';
-?>
+<?php include 'footer_cliente.php'; ?>
