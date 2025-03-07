9e499adef596e6f1e129cac386cf4f8daa506dc8
ValdirDEV143 <prof.valdir.escola@gmail.com>
2025-03-07 14:16:30 -0400

    valdir-nossa-historia-acertopagina

--- controller/nossa_historia.php
+++ controller/nossa_historia.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nossa História</title>
     <link rel="stylesheet" href="../view/public/css/cliente.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> 
 </head>
 <body>
     <header>
