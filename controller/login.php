cd0db259141cf0928fb8b5f6b05e88eec6bdbc16
Viny533 <vinyciusmarques43@gmail.com>
2025-03-10 14:47:53 -0400

    task 10/03/25
    resolvendo problema de include

--- controller/login.php
+++ controller/login.php
@@ -4,18 +4,22 @@ include '../controller/menu-pg-inicial.php';
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../view/public/css/cliente.css">
 </head>
+
 <body>
     <div class="container">
+        
         <div class="info">
-            <img src="../view/public/imagens/logo_john_rooster.png" alt="John Rooster Logo">
-            <p>Aqui você encontra bovinos, galináceos, equinos e diversos produtos para o ramo agropecuário, tudo com qualidade e confiança para o seu negócio.</p>
+                <img src="../view/public/imagens/logo_john_rooster.png" alt="John Rooster Logo">
+                <p>Aqui você encontra bovinos, galináceos, equinos e diversos produtos para o ramo agropecuário, tudo com qualidade e confiança para o seu negócio.</p>
         </div>
+
         <div class="login-form">
             <h2>Login</h2>
             <form action="login.php" method="POST">
@@ -26,12 +30,23 @@ include '../controller/menu-pg-inicial.php';
                 
                 <a href="#">Não tem conta? Cadastre-se</a>
             </form>
+            
         </div>
+        <!-- <div>
+        <?php
+        // include '../controller/footer_cliente.php';
+         ?>
+        </div> -->
     </div>
-    <footer>
-    <?php
-        include '../controller/footer_cliente.php';
-    ?>
-    </footer>
-</body>
+
+
+
+
+
+    <!-- <footer>
+    
+    </footer> -->
+ 
 </html>
+
+</body>
