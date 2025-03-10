abffe46d39f9421502cf57d0d45b225403343bb6
Gabrielbenial <gabrielbenial543@gmail.com>
2025-03-10 15:23:27 -0400

    Update notificacao_ativa.php

--- Controller/notificacao_ativa.php
+++ Controller/notificacao_ativa.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../view/css/notificacao.css">
+    <link rel="stylesheet" href="../view/public/css/cliente.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Notificações</title>
 </head>
