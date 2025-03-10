811e95205312ea03878354ab6aff87de74c5e53a
ValdirDEV143 <prof.valdir.escola@gmail.com>
2025-03-10 16:22:42 -0400

    carrossel-criando-pagina-valdir

---
+++ controller/carrossel_pg_inicial-valdir.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CARROSSEL</title>
+    <link rel="stylesheet" href="../view/public/css/carrossel_pg_inicial-reserva.css">
+</head>
+<body>
+    <div class="container">
+        <h1>
+            <p> <span> Seja Bem-vindo ao nosso site! </span></p> <br>
+            <p><span>Somos um negócio familiar, com anos de experiência no ramo de vendas de animais pecuários, acumulando clientes ao redor de todo o mundo. Temos certeza de que aqui você achará exatamento o(s) animal(is) que procura!</span></p>
+        </h1>
+        <button class="btn">Saiba mais</button>
+        <button class="btn">Categorias</button>
+        <button class="btn">Talvez você goste</button>
+        <button class="btn">Mais vendidos</button>
+
+    </div>
+
+   
+
+    
+</body>
+</html>
