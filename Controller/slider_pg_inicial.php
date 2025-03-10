8495eabab4cefe0687543f09f9c5a5163298143b
isa-miho <isabella.miho@gmail.com>
2025-03-10 16:24:00 -0400

    Criação dos cards

---
+++ Controller/slider_pg_inicial.php
@@ -0,0 +1,28 @@
+<!-- ISABELLA -->
+
+<?php
+include 'menu-pg-inicial.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slider</title>
+    <link rel="stylesheet" href="../view/public/css/cliente.css">
+</head>
+<body>
+    <h2 class="slider_title_pg_inicial">Talvez você goste</h2>
+    <div class="categoria_cards_pg_inicial">
+        <div class="like"></div>
+        <img src="../view/public/imagens/img_slider_pg_inicial/puro_sangue_slider_pg_inicial.jpg">
+    </div>
+
+</body>
+<footer>
+<?php
+    include 'footer_cliente.php';
+?>
+</footer>
+</html>
