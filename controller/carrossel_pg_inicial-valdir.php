47cdcac25c68c91c3200336ae6a5d1bd84dbb615
ValdirDEV143 <prof.valdir.escola@gmail.com>
2025-03-10 17:12:56 -0400

    ajustando-pagina-carrossel-menu-valdir

--- controller/carrossel_pg_inicial-valdir.php
+++ controller/carrossel_pg_inicial-valdir.php
@@ -1,3 +1,7 @@
+<?php
+include 'menu-pg-inicial.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,19 +12,23 @@
 </head>
 <body>
     <div class="container">
-        <h1>
-            <p> <span> Seja Bem-vindo ao nosso site! </span></p> <br>
-            <p><span>Somos um negócio familiar, com anos de experiência no ramo de vendas de animais pecuários, acumulando clientes ao redor de todo o mundo. Temos certeza de que aqui você achará exatamento o(s) animal(is) que procura!</span></p>
-        </h1>
+        <div class="texto-carrosel-pg-inicial">
+            <div class="texto2-img-carrossel-pg-inicial">
+                <img src="../" alt="">
+                <h1 class="texto2-carrossel-pg-inicial"><span class="span-carrossel-pg-ini"> Seja Bem-vindo ao nosso site! </span></h1> 
+                <h2 class="h2-carrossel-pg-inicial">Somos um negócio familiar, com anos de experiência no ramo de vendas de animais pecuários, acumulando clientes ao redor de todo o mundo. Temos certeza de que aqui você achará exatamento o(s) animal(is) que procura!</h2>
+            </div>
+        </div>
         <button class="btn">Saiba mais</button>
-        <button class="btn">Categorias</button>
-        <button class="btn">Talvez você goste</button>
-        <button class="btn">Mais vendidos</button>
-
+        
     </div>
 
    
 
-    
+    <footer>
+        <?php
+        include 'footer_cliente.php';
+        ?>
+    </footer>
 </body>
 </html>
