8c79c82ece8581536a893e10361d681e01976e58
isa-miho <isabella.miho@gmail.com>
2025-03-12 16:48:23 -0400

    Ajuste nas imagens

--- controller/categorias-matheusr.php
+++ controller/categorias-matheusr.php
@@ -11,14 +11,15 @@ include 'menu-pg-inicial.php';
     <link rel="stylesheet" href="../view/public/css/cliente.css">
 </head>
 <body class ='body-math'>
-    <h1 id="pg-categorias-math">Percheron</h1>
+    <h1 id="pg-categorias-math">Talvez você goste</h1>
     <div class="carrossel-cat-math">
         <div class="arrow-cat-math" onclick="prevSlide()">&#10094;</div>
         <div class="cards-cat-math" id="carrossel-cards">
-            <div class="card-cat-math"><img src="horse1.jpg" alt="Horse 1"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
-            <div class="card-cat-math"><img src="horse2.jpg" alt="Horse 2"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
-            <div class="card-cat-math"><img src="horse3.jpg" alt="Horse 3"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
-            <div class="card-cat-math"><img src="horse4.jpg" alt="Horse 4"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
+            <div class="card-cat-math"><img class ="img_slider_math" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg" alt="Horse 1"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
+            <div class="card-cat-math"><img class ="img_slider_math" src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Horse 2"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
+            <div class="card-cat-math"><img class ="img_slider_math" src="../view/public/imagens/img_slider_pg_inicial/painthorse_slider_pg_inicial.jpg" alt="Horse 3"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
+            <div class="card-cat-math"><img class ="img_slider_math" src="../view/public/imagens/img_slider_pg_inicial/puro_sangue_slider_pg_inicial.jpg" alt="Horse 4"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
+            <div class="card-cat-math"><img class ="img_slider_math" src="../view/public/imagens/img_slider_pg_inicial/quarto_de_milha_slider_pq_inicial.jpg" alt="Horse 5"><p>Peso: </p><p>Raça: </p><p>Genealogia: </p><p>Idade: </p><p class="price-cat-math">R$00,00</p><a href="#" class="btn-cat-math">Comprar</a></div>
         </div>
         <div class="arrow-cat-math" onclick="nextSlide()">&#10095;</div>
     </div>
