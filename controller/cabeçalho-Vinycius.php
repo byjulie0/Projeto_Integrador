8da93b2da1fe9820b9765a37dedaefe0fa17543b
Viny533 <vinyciusmarques43@gmail.com>
2025-04-02 15:55:20 -0400

    task

--- controller/cabeçalho-Vinycius.php
+++ controller/cabeçalho-Vinycius.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../view/public/CSS/cliente.css">
     <title>Document</title>
+    <link rel="stylesheet" href="..\view\public\css\cliente.css">
 </head>
 <body>
     <nav class="menu_recuper_senha">
