199d8e9302e0036dce469638922c629d841f238b
mariaaa <toledomiranda.maria23@gmail.com>
2025-04-02 16:23:31 -0400

    menu cadastro - ajustando body

--- controller/menu_cadastro_maria.php
+++ controller/menu_cadastro_maria.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../view/public/CSS/menu_cadastro_maria.css">
+    <link rel="stylesheet" href="../view/public/CSS/cliente.css">
     <title>Document</title>
 </head>
 <body>
