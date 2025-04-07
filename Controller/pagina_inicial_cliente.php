1467f78c94f6322f0b1a579961544a67bb80e5e3
isa-miho <isabella.miho@gmail.com>
2025-04-07 15:03:03 -0400

    Conectando página inicial

---
+++ Controller/pagina_inicial_cliente.php
@@ -0,0 +1,8 @@
+<?php
+include 'menu-pg-inicial.php';
+include 'carrossel_pg_inicial-valdir.php';
+include 'pg_inicial_categorias_isabella.php';
+include 'talvez_voce_goste_mateus.php';
+include 'footer_cliente.php';
+
+?>
