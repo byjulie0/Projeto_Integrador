02e33fab684b366ba7189cce1e49a2c2838b94fa
Eridan <genealerooo@gmail.com>
2025-04-10 17:07:33 -0400

    Criação minha página

---
+++ Controller/redefinir_senha_login.php
@@ -0,0 +1,56 @@
+<!-- Lara -->
+<?php
+include 'menu_recuperar_senha.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redefinir senha</title>
+    <link rel="stylesheet" href="..\view\public\css\cliente.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body class="rede_senha_body">
+
+    <section class="rede_senha_section">
+
+        <nav class="rede_senha_nav_voltar">
+
+            <a href="recuperar_senha_redefinir.php" class="rede_senha_voltar">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+
+        </nav>
+
+        <nav class="rede_senha_nav">
+
+            <div class="recuperar_senha_codigo-content">
+
+                <h1 class="h1_redefinir_senha">Redefinir senha</h1>
+
+                <h3 class="h3_redefinir_senha">Digite sua nova senha </h3>
+
+                <form action="" class="form_rede_senha">
+                    <input type="password" inputmode="numeric" pattern="[0-9]" maxlength="8" required placeholder="Nova senha">
+
+                    <div class="recuperar_senha_codigo-linha_divisao"></div>
+
+                    <input type="password" inputmode="numeric" pattern="[0-9]" maxlength="8" required placeholder="Confirmar nova senha">
+
+                    <button type="submit">Redefinir</button>
+                </form>
+
+            </div>
+        </nav>
+
+    </section>
+
+</body>
+</html>
+
+<?php
+include 'footer_cliente.php';
+?>
