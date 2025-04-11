96106291d70249b497fa4a5e98bfe24e4ce632b6
annax08 <annalaurah768@gmail.com>
2025-04-11 17:03:55 -0400

    em andamento

--- controller/categoria-coluna-de-cards-anna-laurah.php
+++ controller/categoria-coluna-de-cards-anna-laurah.php
@@ -4,12 +4,14 @@ include 'menu_pg_inicial.php';
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Percheron</title>
   <link rel="stylesheet" href="../view/public/css/cliente.css">
 </head>
+
 <body class="body-coluna-de-cards">
   <a href="#" class="voltar"></a>
 
@@ -17,51 +19,52 @@ include 'menu_pg_inicial.php';
     <h2 class="titulo-categoria-coluna-de-cards">Percheron</h2>
     <div class="cards-coluna-de-cards">
       <div class="card-coluna-de-cards">
-        <img src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Percheron 1">
-        <p><strong>Peso:</strong> <span>-</span></p>
-        <p><strong>Raça:</strong> <span>Percheron</span></p>
-        <p><strong>Genealogia:</strong> <span>-</span></p>
-        <p><strong>Idade:</strong> <span>-</span></p>
-        <h3>R$00,00</h3>
-        <button>Comprar</button>
+        <img class="" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
+          alt="Percheron 1">
+        <p>Peso: </p>
+        <p>Raça: </p>
+        <p>Genealogia: </p>
+        <p>Idade: </p>
+        <p class="preco-coluna-card">R$00,00</p>
+        <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Percheron 1">
-        <p><strong>Peso:</strong> <span>-</span></p>
-        <p><strong>Raça:</strong> <span>Percheron</span></p>
-        <p><strong>Genealogia:</strong> <span>-</span></p>
-        <p><strong>Idade:</strong> <span>-</span></p>
-        <h3>R$00,00</h3>
-        <button>Comprar</button>
+        <img class="" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
+          alt="Percheron 1">
+        <p>Peso: </p>
+        <p>Raça: </p>
+        <p>Genealogia: </p>
+        <p>Idade: </p>
+        <p class="preco-coluna-card">R$00,00</p>
+        <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Percheron 1">
-        <p><strong>Peso:</strong> <span>-</span></p>
-        <p><strong>Raça:</strong> <span>Percheron</span></p>
-        <p><strong>Genealogia:</strong> <span>-</span></p>
-        <p><strong>Idade:</strong> <span>-</span></p>
-        <h3>R$00,00</h3>
-        <button>Comprar</button>
+        <img class="" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
+          alt="Percheron 1">
+        <p>Peso: </p>
+        <p>Raça: </p>
+        <p>Genealogia: </p>
+        <p>Idade: </p>
+        <p class="preco-coluna-card">R$00,00</p>
+        <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Percheron 1">
-        <p><strong>Peso:</strong> <span>-</span></p>
-        <p><strong>Raça:</strong> <span>Percheron</span></p>
-        <p><strong>Genealogia:</strong> <span>-</span></p>
-        <p><strong>Idade:</strong> <span>-</span></p>
-        <h3>R$00,00</h3>
-        <button>Comprar</button>
+        <img class="" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
+          alt="Percheron 1">
+        <p>Peso: </p>
+        <p>Raça: </p>
+        <p>Genealogia: </p>
+        <p>Idade: </p>
+        <p class="preco-coluna-card">R$00,00</p>
+        <button class="btn-coluna-card">Comprar</button>
       </div>
-      <div class="card-coluna-de-cards">
-        <img src="../view/public/imagens/img_slider_pg_inicial/mustang_slider_pg_inicial.jpg" alt="Percheron 1">
-        <p><strong>Peso:</strong> <span>-</span></p>
-        <p><strong>Raça:</strong> <span>Percheron</span></p>
-        <p><strong>Genealogia:</strong> <span>-</span></p>
-        <p><strong>Idade:</strong> <span>-</span></p>
-        <h3>R$00,00</h3>
-        <button>Comprar</button>
-      </div>
+      
     </div>
   </div>
 </body>
+
 </html>
+
+<?php
+// include 'footer_cliente.php';
+?>
