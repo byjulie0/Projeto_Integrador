bdb6c75e92db37cdb4fd142cfbfb789f44ec6701
mariaaa <toledomiranda.maria23@gmail.com>
2025-04-29 14:51:55 -0400

    front coluna de card finalizado

--- controller/categoria-coluna-de-cards-anna-laurah.php
+++ controller/categoria-coluna-de-cards-anna-laurah.php
@@ -14,51 +14,53 @@ include 'menu_pg_inicial.php';
 
 <body class="body-coluna-de-cards">
   <a href="#" class="voltar"></a>
-
+  <h2 class="titulo-categoria-coluna-de-cards"><i class="bi bi-chevron-left"></i>Raça do Cavalo</h2>
   <div class="container-coluna-de-cards">
-    <h2 class="titulo-categoria-coluna-de-cards">Percheron</h2>
     <div class="cards-coluna-de-cards">
       <div class="card-coluna-de-cards">
-        <img class="imagem-coluna-de-cards" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
-          alt="Percheron 1">
-        <p>Peso: </p>
-        <p>Raça: </p>
-        <p>Genealogia: </p>
-        <p>Idade: </p>
+        <img class="imagem-coluna-de-cards" src="../view/public/Imagens/coluna_de_card/cavalo_dourado.jpg"alt="Percheron 1">
+        <div class="detalhes-card-coluna">
+          <p>Peso: </p>
+          <p>Raça: </p>
+          <p>Genealogia: </p>
+          <p>Idade: </p>
+        </div>
         <p class="preco-coluna-card">R$00,00</p>
         <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img class="imagem-coluna-de-cards" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
-          alt="Percheron 1">
-        <p>Peso: </p>
-        <p>Raça: </p>
-        <p>Genealogia: </p>
-        <p>Idade: </p>
+        <img class="imagem-coluna-de-cards" src="../view/public/Imagens/coluna_de_card/cavalo_dourado.jpg"alt="Percheron 1">
+        <div class="detalhes-card-coluna">
+          <p>Peso: </p>
+          <p>Raça: </p>
+          <p>Genealogia: </p>
+          <p>Idade: </p>
+        </div>
         <p class="preco-coluna-card">R$00,00</p>
         <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img class="imagem-coluna-de-cards" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
-          alt="Percheron 1">
-        <p>Peso: </p>
-        <p>Raça: </p>
-        <p>Genealogia: </p>
-        <p>Idade: </p>
+        <img class="imagem-coluna-de-cards" src="../view/public/Imagens/coluna_de_card/cavalo_dourado.jpg"alt="Percheron 1">
+        <div class="detalhes-card-coluna">
+          <p>Peso: </p>
+          <p>Raça: </p>
+          <p>Genealogia: </p>
+          <p>Idade: </p>
+        </div>
         <p class="preco-coluna-card">R$00,00</p>
         <button class="btn-coluna-card">Comprar</button>
       </div>
       <div class="card-coluna-de-cards">
-        <img class="imagem-coluna-de-cards" src="../view/public/imagens/img_slider_pg_inicial/cavalo_arabe_slider_pg_inicial.jpg"
-          alt="Percheron 1">
-        <p>Peso: </p>
-        <p>Raça: </p>
-        <p>Genealogia: </p>
-        <p>Idade: </p>
+        <img class="imagem-coluna-de-cards" src="../view/public/Imagens/coluna_de_card/cavalo_dourado.jpg"alt="Percheron 1">
+        <div class="detalhes-card-coluna">
+          <p>Peso: </p>
+          <p>Raça: </p>
+          <p>Genealogia: </p>
+          <p>Idade: </p>
+        </div>
         <p class="preco-coluna-card">R$00,00</p>
         <button class="btn-coluna-card">Comprar</button>
       </div>
-      
     </div>
   </div>
 </body>
@@ -66,5 +68,5 @@ include 'menu_pg_inicial.php';
 </html>
 
 <?php
-// include 'footer_cliente.php';
+include 'footer_cliente.php';
 ?>
