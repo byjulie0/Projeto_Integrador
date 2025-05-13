23144f2cb6929b4b0c686732ed1b04d98b1327d4
mariaaa <toledomiranda.maria23@gmail.com>
2025-05-13 14:41:41 -0400

    iniciando add produtos segunda etapa

---
+++ controller/adm/add_produtos_segunda_etapa.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Produto</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../view/public/css/adm.css">
+</head>
+<body class="body-add-produtos-2-etapa">
+    <div class="area-titulo-add-produtos-2-etapa">
+        <a href="adicionar_produto.php" class="icon-voltar-add-produtos-2-etapa">
+            <i class="fa-solid fa-chevron-left"></i>
+        </a>
+        <h1 class="titulo-add-produtos-2-etapa">Adicionar Produto</h1>
+    </div>
+    <p class="info-add-produtos-2-etapa">Preencha as informações necessárias e adicione produtos ao catálogo do site</p>
+    <section class="area-add-produto-2-etapa">
+        <div class="area-add-imagem-add-produtos-2-etapa">
+            
+        </div>
+    </section>
+</body>
+</html>
