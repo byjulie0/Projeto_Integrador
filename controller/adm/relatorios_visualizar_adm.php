c539a50037d72bc2efa08960e15b79d5c4b50f04
Den09hub <denyel.1005@gmail.com>
2025-05-26 17:06:33 -0400

    Relatorios_Visualizar_task1
    Data 26/05/2025 implementei a estrutura do HTML, com algumas tags básicas como a main, div e section (e outros mais). Coloquei o header e footer da página, e links no head.

---
+++ controller/adm/relatorios_visualizar_adm.php
@@ -0,0 +1,39 @@
+<?php
+include "../cliente/menu_pg_inicial.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatórios</title>
+    <link rel="stylesheet" href="/PROJETO_INTEGRADOR/view/public/css/adm.css">
+    <link rel="stylesheet" href="/PROJETO_INTEGRADOR/view/public/css/cliente.css">
+    <link rel="stylesheet" href="https://fontawesome.com/icons/chevron-left?f=classic&s=solid">
+</head>
+<body>
+    <main>
+        <a href="" class="verificar_administrar_pedidos_sessao_seta_voltar">
+            <i class="fa-solid fa-chevron-left"></i>
+        </a>
+        <div>
+            <h1>Visualizar relatórios</h1>
+            <h3>Mostrando relatórios referentes ao período: <span>XX/XX/XXXX - YY/YY/YYYY</span></h3>
+            <h3>Mudar período</h3>
+        </div>
+        <section>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    </main>
+</body>
+</html>
+
+<?php
+include "../adm/footer_adm.php";
+?>
