a39e8d7a0460948a6f9cd0ced2ff1e8e330dc6cf
mariaaa <toledomiranda.maria23@gmail.com>
2025-05-28 16:59:55 -0400

    arrumando a disposição

--- controller/adm/add_produtos_segunda_etapa.php
+++ controller/adm/add_produtos_segunda_etapa.php
@@ -9,37 +9,37 @@
     <link rel="stylesheet" href="../../view/public/css/adm.css">
 </head>
 
-<body class="body_add_product">
-    <div class="title_page_add_product">
-        <a href="#" class="arrow_add_product">
+<body class="body_add_product_second">
+    <div class="title_page_add_product_second">
+        <a href="#" class="arrow_add_product_second">
             <i class="fa-solid fa-chevron-left">
             </i>
         </a>
-        <h1 class="tile_add_product">Adicionar Produto</h1>
+        <h1 class="tile_add_product_second">Adicionar Produto</h1>
     </div>
-    <p class="info_add_product">Preencha as informações necessárias e adicione produtos ao catálogo do site</p>
-    <section class="add_product_area">
-        <article class="add_product_image">
-            <p class="product_title_info">Carregar imagem de capa<span class="mandatory_space">*</span></p>
-            <div class="img_holder">
-                <label class="img_holder_button">
+    <p class="info_add_product_second">Preencha as informações necessárias e adicione produtos ao catálogo do site</p>
+    <section class="add_product_area_second">
+        <article class="add_product_image_second">
+            <p class="product_title_info_second">Carregar imagem de capa<span class="mandatory_space_second">*</span></p>
+            <div class="img_holder_second">
+                <label class="img_holder_button_second">
                     <i class="fa-solid fa-arrow-up-from-bracket"></i>
                     <span>Selecione uma imagem</span>
                     <input type="file" accept="image/*" style="display: none;">
                 </label>
             </div>
         </article>
-        <aside class="add_product_details">
-            <div class="product_details_collumn">
-                <article class="input_product_name">
-                    <p class="product_title_info">Insira o nome do produto<span class="mandatory_space">*</span></p>
-                    <input type="text" class="input_product_info" placeholder="Titulo" required>
-                    <span class="error-message">Por favor, preencha este campo</span>
+        <aside class="add_product_details_second">
+            <div class="product_details_collumn_second">
+                <article class="input_product_price_second">
+                    <p class="product_title_info_second">Defina o valor do produto<span class="mandatory_space_second">*</span></p>
+                    <input type="number" placeholder="Valor" class="input_product_info_second" required min="0.01" step="0.01">
+                    <span class="error-message_second">Informe um valor válido (maior que zero)</span>
                 </article>
-                <article class="input_product_category">
-                    <p class="product_title_info">Selecione a categoria a qual o produto pertence<span
-                            class="mandatory_space">*</span></p>
-                    <select name="categories" id="categories" class="product_info_select" required>
+                <article class="input_product_category_second">
+                    <p class="product_title_info_second">Selecione a categoria a qual o produto pertence<span
+                            class="mandatory_space_second">*</span></p>
+                    <select name="categories" id="categories" class="product_info_select_second" required>
                         <option value="" selected disabled>Selecione uma categoria</option>
                         <option value="bovinos" class="product_categories">Bovinos</option>
                         <option value="equinos" class="product_categories">Equinos</option>
@@ -49,75 +49,11 @@
                     </select>
                     <span class="error-message">Por favor, selecione uma categoria</span>
                 </article>
-                <article class="input_product_subcategory">
-                    <p class="product_title_info">Selecione a subcategoria a qual o produto pertence<span
-                            class="mandatory_space">*</span></p>
-                    <select name="subcategories" id="bovinos_breed" class="product_info_select subcategory-select"
-                        required>
-                        <option value="" selected disabled>Selecione uma subcategoria</option>
-                        <option value="angus" class="product_bull">Angus</option>
-                        <option value="hereford" class="product_bull">Hereford</option>
-                        <option value="nelore" class="product_bull">Nelore</option>
-                        <option value="girolando" class="product_bull">Girolando</option>
-                        <option value="brahman" class="product_bull">Brahman</option>
-                    </select>
-                    <select name="subcategories" id="equinos_breed" class="product_info_select subcategory-select"
-                        required>
-                        <option value="" selected disabled>Selecione uma subcategoria</option>
-                        <option value="puro_sangue" class="product_horse">Puro Sangue Inglês</option>
-                        <option value="andaluz" class="product_horse">Andaluz</option>
-                        <option value="arabe" class="product_horse">Árabe</option>
-                        <option value="lusitano" class="product_horse">Lusitano</option>
-                        <option value="marchador" class="product_horse">Manfalarga Marchador</option>
-                    </select>
-                    <select name="subcategories" id="galinaceos_breed" class="product_info_select subcategory-select"
-                        required>
-                        <option value="" selected disabled>Selecione uma subcategoria</option>
-                        <option value="brahma" class="product_rooster">Brahma</option>
-                        <option value="orpington" class="product_rooster">Orpington</option>
-                        <option value="sussex" class="product_rooster">Sussex</option>
-                        <option value="playmouth" class="product_rooster">Playmouth Rock</option>
-                        <option value="rhode_island" class="product_rooster">Rhode Island Redd</option>
-                    </select>
-                    <select name="subcategories" id="product_types" class="product_info_select subcategory-select"
-                        required>
-                        <option value="" selected disabled>Selecione uma subcategoria</option>
-                        <option value="racao" class="general_product">Rações e suplementos alimentares</option>
-                        <option value="hereford" class="general_product">Medicamentos veterinários</option>
-                        <option value="nelore" class="general_product">Produtos de higiene e cuidados</option>
-                        <option value="girolando" class="general_product">Equipamentos e utensílios</option>
-                        <option value="brahman" class="general_product">Suplementos nutricionais e aditivos</option>
-                    </select>
-                    <select name="subcategories" id="winner_breed" class="product_info_select subcategory-select"
-                        required>
-                        <option value="" selected disabled>Selecione uma subcategoria</option>
-                        <option value="melhor_exemplar" class="champion_product">Melhor exemplar</option>
-                        <option value="grande_campeao" class="champion_product">Grande Campeão</option>
-                        <option value="campeao_junior" class="champion_product">Campeão júnior</option>
-                        <option value="melhor_desempenho" class="champion_product">Melhor desempenho Funcional</option>
-                        <option value="melhor_apresentacao" class="champion_product">Melhor apresentação</option>
-                    </select>
-                </article>
-            </div>
-            <div class="product_details_collumn">
-                <article class="input_product_price">
-                    <p class="product_title_info">Defina o valor do produto<span class="mandatory_space">*</span></p>
-                    <input type="number" placeholder="Valor" class="input_product_info" required min="0.01" step="0.01">
-                    <span class="error-message">Informe um valor válido (maior que zero)</span>
-                </article>
-                <article class="input_product_quantity">
-                    <p class="product_title_info">Quantidade em estoque<span class="mandatory_space">*</span></p>
-                    <input type="number" placeholder="Valor" class="input_product_info" required min="1">
-                </article>
-                <article class="input_product_quantity">
-                    <p class="product_title_info">Descreva o produto<span class="mandatory_space">*</span></p>
-                    <input type="text" placeholder="Digite aqui" class="input_product_info product_details" required>
-                </article>
             </div>
         </aside>
     </section>
-    <div class="add_product_submit_button">
-        <button type="submit" class="add_product_button">
+    <div class="add_product_submit_button_second">
+        <button type="submit" class="add_product_button_second">
             Avançar
             <i class="fa-solid fa-arrow-right"></i>
         </button>
@@ -125,3 +61,7 @@
 </body>
 
 </html>
+
+<?php
+include 'footer_adm.php';
+?>
