73443d0b41e26e76ee04ab82325f19ba802d740c
Den09hub <denyel.1005@gmail.com>
2025-06-04 16:56:00 -0400

    Relatorios_Visualizar_task3

--- controller/adm/relatorios_visualizar_adm.php
+++ controller/adm/relatorios_visualizar_adm.php
@@ -22,13 +22,39 @@ include "../adm/menu_adm.php";
             <h3>Mostrando relatórios referentes ao período: <span>XX/XX/XXXX - YY/YY/YYYY</span></h3>
             <h3>Mudar período</h3>
         </div>
-        <section>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <section class="dn_section">
+            <div class="dn_views">
+                <div class="dn_box-img"><img src="../imagens/View.svg" alt=""></div>
+                <p class="coisa">visualizaçoes</p>
+                <p class="coisa2">totais do site:</p>  <img class="dn_img"src="" alt="">
+                <p class="coisa3">0</p>
+            </div>
+            <div class="dn_sales">
+                <div class="dn_box-img2"><img src="" alt=""></div>
+                <p>Vendas:0</p>  <img class="dn_img2"src="" alt="">
+                
+            </div>
+            <div class="dn_total-products">
+                <div class="dn_box-img3"><img src="" alt=""></div>
+                <p>Total de</p>
+                <p>produtos cadastrados:</p>  <img class="dn_img3"src="" alt="">
+                <p>0</p>
+            </div>
+            <div class="dn_total-users">
+                <div class="dn_box-img4"><img src="" alt=""></di>
+                <p>Usuários</p>
+                <p>cadastrados:</p>  <img class="dn_img4"src="" alt="">
+                <p>0</p>
+            </div>
+            <div class="dn_sales-today">
+
+            </div>
+            <div class="dn_statics">
+
+            </div>
+            <div class="dn_atividade">
+
+            </div>
         </section>
     </main>
 </body>
