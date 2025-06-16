3d2a90e7a85f9ca37d1f046186e0ad4bd3b39883
Den09hub <denyel.1005@gmail.com>
2025-06-16 17:07:40 -0400

    Relatorios_Visualizar_task4

--- controller/adm/relatorios_visualizar_adm.php
+++ controller/adm/relatorios_visualizar_adm.php
@@ -12,50 +12,29 @@ include "../adm/menu_adm.php";
     <link rel="stylesheet" href="/PROJETO_INTEGRADOR/view/public/css/cliente.css">
     <link rel="stylesheet" href="https://fontawesome.com/icons/chevron-left?f=classic&s=solid">
 </head>
-<body>
+<body class="verificar_administrar_pedidos_sessao_body">
     <main>
-        <a href="" class="verificar_administrar_pedidos_sessao_seta_voltar">
-            <i class="fa-solid fa-chevron-left"></i>
-        </a>
-        <div>
+        <div class="verificar_administrar_pedidos_sessao_info">
+            <a href="" class="verificar_administrar_pedidos_sessao_seta_voltar">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
             <h1>Visualizar relatórios</h1>
-            <h3>Mostrando relatórios referentes ao período: <span>XX/XX/XXXX - YY/YY/YYYY</span></h3>
-            <h3>Mudar período</h3>
+            <div class="verificar_administrar_pedidos_sessao_mini_titulos">
+                <h3>Mostrando relatórios referentes ao período: <span class="verificar_administrar_pedidos_sessao_titulo_destaque">XX/XX/XXXX - YY/YY/YYYY</span></h3>
+                <h3>Mudar período</h3>
+            </div>
         </div>
-        <section class="dn_section">
-            <div class="dn_views">
-                <div class="dn_box-img"><img src="../imagens/View.svg" alt=""></div>
-                <p class="coisa">visualizaçoes</p>
-                <p class="coisa2">totais do site:</p>  <img class="dn_img"src="" alt="">
-                <p class="coisa3">0</p>
-            </div>
-            <div class="dn_sales">
-                <div class="dn_box-img2"><img src="" alt=""></div>
-                <p>Vendas:0</p>  <img class="dn_img2"src="" alt="">
-                
-            </div>
-            <div class="dn_total-products">
-                <div class="dn_box-img3"><img src="" alt=""></div>
-                <p>Total de</p>
-                <p>produtos cadastrados:</p>  <img class="dn_img3"src="" alt="">
-                <p>0</p>
-            </div>
-            <div class="dn_total-users">
-                <div class="dn_box-img4"><img src="" alt=""></di>
-                <p>Usuários</p>
-                <p>cadastrados:</p>  <img class="dn_img4"src="" alt="">
-                <p>0</p>
-            </div>
-            <div class="dn_sales-today">
-
-            </div>
-            <div class="dn_statics">
-
-            </div>
-            <div class="dn_atividade">
-
-            </div>
+        
+        <section>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
         </section>
+
     </main>
 </body>
 </html>
