d01f184dfa76c4e5e601172f13055e029dc0e769
Denyel09Dev <denyel.1005@gmail.com>
2025-06-21 23:06:39 -0300

    Relatorios_Visualizar_task7

--- controller/adm/relatorios_visualizar_adm.php
+++ controller/adm/relatorios_visualizar_adm.php
@@ -27,19 +27,37 @@ include "../adm/menu_adm.php";
             <div>
                 <div class="verificar_administrar_pedidos_sessao_coluna_esquerda">
                     <div class="verificar_administrar_pedidos_sessao_coluna">
-                        <div class="verificar_administrar_pedidos_sessao_bloco"></div>
-                        <div class="verificar_administrar_pedidos_sessao_bloco_segundo"></div>
+                        <div class="verificar_administrar_pedidos_sessao_bloco">
+                            <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
+                            <h5>Visualizações <br>totais do site: <span><br>0</span></h5>
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div class="verificar_administrar_pedidos_sessao_bloco">
+                            <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
+                            <h5>Vendas: <span><br>0</span></h5>
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
                     </div>
                     <div class="verificar_administrar_pedidos_sessao_coluna">
-                        <div class="verificar_administrar_pedidos_sessao_bloco"></div>
-                        <div class="verificar_administrar_pedidos_sessao_bloco_segundo"></div>
+                        <div class="verificar_administrar_pedidos_sessao_bloco">
+                            <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
+                            <h5>Total de produtos cadastrados: <span><br>0</span></h5>
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div class="verificar_administrar_pedidos_sessao_bloco">
+                            <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
+                            <h5>Usuários cadastrados: <span><br>0</span></h5>
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
                     </div>
                 </div>
                 <div class="verificar_administrar_pedidos_sessao_estatisticas"></div>
             </div>
             <div class="verificar_administrar_pedidos_sessao_coluna_direita">
                 <div class="verificar_administrar_pedidos_sessao_bloco_vendas"></div>
-                <div class="verificar_administrar_pedidos_sessao_bloco_atividades"></div>
+                <div class="verificar_administrar_pedidos_sessao_bloco_atividades">
+                    <h4>Atividades recentes: </h4>
+                </div>
             </div>
         </section>
 
