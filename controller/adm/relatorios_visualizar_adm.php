5544dd1548174aaf74629171e0b1a160cea76702
Den09hub <denyel.1005@gmail.com>
2025-06-23 17:11:19 -0400

    Relatorios_Visualizar_task8
    Na data 23/06/2025 coloquei as informações nos quadros referente ao figma, ajustando e adicionando tags, juntamente estilizando.

--- controller/adm/relatorios_visualizar_adm.php
+++ controller/adm/relatorios_visualizar_adm.php
@@ -29,34 +29,123 @@ include "../adm/menu_adm.php";
                     <div class="verificar_administrar_pedidos_sessao_coluna">
                         <div class="verificar_administrar_pedidos_sessao_bloco">
                             <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
-                            <h5>Visualizações <br>totais do site: <span><br>0</span></h5>
+                            <h5>Visualizações <br>totais do site: <span class="verificar_administrar_pedidos_sessao_zero"><br><h4>0</h4></span></h5>
                             <i class="fa-solid fa-chevron-left"></i>
                         </div>
                         <div class="verificar_administrar_pedidos_sessao_bloco">
                             <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
-                            <h5>Vendas: <span><br>0</span></h5>
+                            <h5>Vendas: <span class="verificar_administrar_pedidos_sessao_zero"><br>0</span></h5>
                             <i class="fa-solid fa-chevron-left"></i>
                         </div>
                     </div>
                     <div class="verificar_administrar_pedidos_sessao_coluna">
                         <div class="verificar_administrar_pedidos_sessao_bloco">
                             <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
-                            <h5>Total de produtos cadastrados: <span><br>0</span></h5>
+                            <h5>Total de produtos cadastrados: <span class="verificar_administrar_pedidos_sessao_zero"><br>0</span></h5>
                             <i class="fa-solid fa-chevron-left"></i>
                         </div>
                         <div class="verificar_administrar_pedidos_sessao_bloco">
                             <div class="verificar_administrar_pedidos_sessao_bloco_mini"></div>
-                            <h5>Usuários cadastrados: <span><br>0</span></h5>
+                            <h5>Usuários <br>cadastrados: <span class="verificar_administrar_pedidos_sessao_zero"><br>0</span></h5>
                             <i class="fa-solid fa-chevron-left"></i>
                         </div>
                     </div>
                 </div>
-                <div class="verificar_administrar_pedidos_sessao_estatisticas"></div>
+                <div class="verificar_administrar_pedidos_sessao_estatisticas">
+                    <h4>Estatísticas</h4>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>20%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>18%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>15%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>12%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>10%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>5%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>2%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_horizontais">
+                        <h6>1%</h6>
+                        <hr>
+                    </div>
+
+                    <div class="verificar_administrar_pedidos_sessao_estatisticas_linhas_verticais">
+                        <h6>D</h6>
+                        <h6>S</h6>
+                        <h6>T</h6>
+                        <h6>Q</h6>
+                        <h6>Q</h6>
+                        <h6>S</h6>
+                        <h6>S</h6>
+                    </div>
+                </div>
             </div>
             <div class="verificar_administrar_pedidos_sessao_coluna_direita">
-                <div class="verificar_administrar_pedidos_sessao_bloco_vendas"></div>
+                <div class="verificar_administrar_pedidos_sessao_bloco_vendas">
+                    <h4>Vendas hoje</h4>
+                    <h3>R$:0,00</h3>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>20%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>18%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>15%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>12%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>10%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>5%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>2%</h6>
+                        <hr>
+                    </div>
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_horizontais">
+                        <h6>1%</h6>
+                        <hr>
+                    </div>
+
+                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_verticais">
+                        <h6>D</h6>
+                        <h6>S</h6>
+                        <h6>T</h6>
+                        <h6>Q</h6>
+                        <h6>Q</h6>
+                        <h6>S</h6>
+                        <h6>S</h6>
+                    </div>
+                </div>
                 <div class="verificar_administrar_pedidos_sessao_bloco_atividades">
-                    <h4>Atividades recentes: </h4>
+                    <h4>Atividades <br>recentes</h4>
                 </div>
             </div>
         </section>
