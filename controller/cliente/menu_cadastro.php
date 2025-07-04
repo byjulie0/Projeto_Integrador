1d6b74149297d8da8f56f8c896f130fa22650b80
MariArantes4 <arantesmariana839@gmail.com>
2025-07-04 14:53:17 -0400

    edições da responsividade

--- controller/cliente/menu_cadastro.php
+++ controller/cliente/menu_cadastro.php
@@ -1,20 +1,11 @@
-<!-- MARIA HELENA -->
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../view/public/css/cliente.css">
-    <title>John Rooster - Cadastro </title>
-</head>
-<body>
-    <nav class="menu-cadastro">
-        <div class="info-menu-cadastro">
-            <div class="logo-menu-cadastro">
-                <img src="../../view/public/imagens/img_logofabrica.png" alt="" class="logo-menu-cadastro">
-            </div>
-            <p class="nome-menu-cadastro">John Rooster - Cadastro</p>
-        </div>
-    </nav>
-</body>
-</html>
+<?php
+// menu_cadastro.php
+?>
+<nav class="menu-cadastro">
+  <div class="info-menu-cadastro">
+    <div class="logo-menu-cadastro">
+      <img src="../../view/public/imagens/img_logofabrica.png" alt="Logo John Rooster">
+    </div>
+    <div class="nome-menu-cadastro">John Rooster - Cadastro</div>
+  </div>
+</nav>
