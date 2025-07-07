c9d15a51fe3adc889c7841c09a69486d30d682c2
Den09hub <denyel.1005@gmail.com>
2025-07-07 17:04:33 -0400

    Relatorios_Visualizar_task15

--- controller/adm/relatorios_visualizar_adm.php
+++ controller/adm/relatorios_visualizar_adm.php
@@ -150,16 +150,16 @@ include "../adm/menu_adm.php";
                             <h6>1%</h6>
                             <hr>
                         </div>
-                    </div>
 
-                    <div class="verificar_administrar_pedidos_sessao_vendas_linhas_verticais">
-                        <h6>D</h6>
-                        <h6>S</h6>
-                        <h6>T</h6>
-                        <h6>Q</h6>
-                        <h6>Q</h6>
-                        <h6>S</h6>
-                        <h6>S</h6>
+                        <div class="verificar_administrar_pedidos_sessao_vendas_linhas_verticais">
+                            <h6>D</h6>
+                            <h6>S</h6>
+                            <h6>T</h6>
+                            <h6>Q</h6>
+                            <h6>Q</h6>
+                            <h6>S</h6>
+                            <h6>S</h6>
+                        </div>
                     </div>
                 </div>
                 <div class="verificar_administrar_pedidos_sessao_bloco_atividades">
