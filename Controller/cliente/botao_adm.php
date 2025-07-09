e70e1f5329f9b4bc9ed258e8026842d0ab2e8cf7
Eridan <genealerooo@gmail.com>
2025-07-08 21:30:02 -0400

    Remoção de lixos e ajustes

--- Controller/cliente/botao_adm.php
+++ Controller/cliente/botao_adm.php
@@ -1,5 +1,8 @@
+<?php
+$texto = 'ADM';
+?>
 <div class="generate_report_submit_button">
-        <button type="submit" class="generate_report_product_button">
-        <?= $texto ?>
-        </button>
-    </div>
+    <button type="submit" class="generate_report_product_button">
+    <?= $texto ?>
+    </button>
+</div>
