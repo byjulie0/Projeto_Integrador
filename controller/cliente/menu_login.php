cdfa46720ed89fac912dff58aad6e03ea31bf672
Eridan <genealerooo@gmail.com>
2025-07-08 23:56:09 -0400

    Ajustes cadastro e login para ficarem padronizados

--- controller/cliente/menu_login.php
+++ controller/cliente/menu_login.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../view/public/css/cliente.css">
     <title>Document</title>
     <link rel="stylesheet" href="/Projeto_Integrador/view/public/css/cliente.css">
 </head>
@@ -18,4 +17,3 @@
     </nav>
 </body>
 </html>
-<!-- View\Public\Imagens\img_logofabrica.png -->
