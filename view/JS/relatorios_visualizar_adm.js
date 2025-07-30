ed2d3116fcf02993a09bb8159c141672f539f836
Denyel09Dev <denyel.1005@gmail.com>
2025-07-30 17:03:49 -0400

    Relatorios visualizar adm Denyel (#110)
    * Relatorios_Visualizar_task26
    
    * Relatorios_Visualizar_task27
    
    * Relatorios_Visualizar_task28
    
    * Relatorios_Visualizar_task29
    
    * Relatorios_Visualizar_task30

--- view/JS/relatorios_visualizar_adm.js
+++ view/JS/relatorios_visualizar_adm.js
@@ -1,3 +1,58 @@
-document.getElementById('cliqueAqui').addEventListener('click', function() {
-    document.getElementById('dataInput').focus();
+// Início do Calendário
+
+$(function () {
+
+  // Configuração manual regional do calendário
+  $.datepicker.regional['pt-BR'] = {
+      closeText: 'Fechar',
+      prevText: '&#x3C;Anterior',
+      nextText: 'Próximo&#x3E;',
+      currentText: 'Hoje',
+      monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+          'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+      monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+          'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+      dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+      dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+      dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+      weekHeader: 'Sem',
+      dateFormat: 'dd/mm/yy',
+      firstDay: 0,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
+  };
+
+  $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+
+  // Inicia os dois calendários
+
+  $('#dataInicio').datepicker({
+      onSelect: function () {
+          atualizarPeriodo();
+      }
   });
+
+  $('#dataFim').datepicker({
+      onSelect: function () {
+          atualizarPeriodo();
+      }
+  });
+
+  $('#abrirCalendario').on('click', function () {
+      $('#dataInicio, #dataFim').datepicker('show');
+      $('#dataInicio').show();
+      $('#dataFim').show();
+  });
+
+  function atualizarPeriodo() {
+      var inicio = $('#dataInicio').val();
+      var fim = $('#dataFim').val();
+
+      if (inicio && fim) {
+          $('#dataEscolhida').text(inicio + ' - ' + fim);
+      }
+  }
+});
+
+// Fim do Calendário
