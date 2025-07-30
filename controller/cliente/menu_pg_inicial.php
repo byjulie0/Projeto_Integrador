d9f81079ed1e8932161f2d175612ce611c3041e3
byjulie0 <julievieiramartins0@gmail.com>
2025-07-30 15:04:22 -0400

    adicionando id

--- controller/cliente/menu_pg_inicial.php
+++ controller/cliente/menu_pg_inicial.php
@@ -43,7 +43,7 @@
                     </ul>
 
                 </li>
-                <a href="" class="nav-item-pg-inicial">Campeões do mês</a>
+                <a href="#campeoesdomes" class="nav-item-pg-inicial">Campeões do mês</a>
                 <a href="#maisvendidos" class="nav-item-pg-inicial">Mais vendidos</a>
             </div>
 
