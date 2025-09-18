ea51666ebb49cbd58dda2ec730fb7903fc6829ec
mariaaa <toledomiranda.maria23@gmail.com>
2025-09-18 17:07:49 -0400

    responsividade do histórico de compra

--- Controller/cliente/historico_de_compras.php
+++ Controller/cliente/historico_de_compras.php
@@ -1,50 +1,46 @@
 <?php include 'menu_pg_inicial.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Histórico de Compras</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../../view/public/css/cliente/historico_de_compras.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="../../view/public/css/cliente/historico_de_compras.css" />
 </head>
 <body class="body_historico_compras">
     <div class="container_historico_compras">
- 
+
         <div class="title_historico_compras">
             <a href="#" onclick="window.history.back(); return false;" class="arrow_compras">
-                <i class="fa-solid fa-chevron-left">
-                </i>
+                <i class="fa-solid fa-chevron-left"></i>
             </a>
             <h1 class="titulo_historico_compras">Histórico de Compras</h1>
         </div>
         
         <div class="area_historico_compras">
-            
-           <table class="table_historico_compras">
-                <tr class="row_info row_header">
-                    <th class="th_data_compras">Data do Pedido:</th>
-                    <th class="th_botao">
-                        <?php
-                            $texto = "Cancelar";
-                            include 'botao_cancelar.php';
-                        ?>
-                    </th>
-                </tr>
 
-                <tr class="row_info row_detalhes">
-                    <th class="th_codigo_compras">Código do pedido</th>
-                    <th class="th_historico_compras">Total de itens</th>
-                    <th class="th_historico_compras">Valor do pedido</th>
-                    <th class="status_historico_compras">Status do pedido</th>
-                </tr>
-            </table>
+            <div class="pedido_header">
+                <p class="data_pedido">Data do Pedido:</p>
+                <div class="botao_cancelar">
+                    <?php
+                        $texto = "Cancelar";
+                        include 'botao_cancelar.php';
+                    ?>
+                </div>
+            </div>
+
+            <div class="pedido_detalhes">
+                <p class="codigo_pedido">Código do pedido</p>
+                <p class="total_itens">Total de itens</p>
+                <p class="valor_pedido">Valor do pedido</p>
+                <p class="status_pedido">Status do pedido</p>
+            </div>
+
         </div>
     </div>
-    
+
+<?php include 'footer_cliente.php'; ?>
 </body>
 </html>
-<?php
-include 'footer_cliente.php';
-?>
