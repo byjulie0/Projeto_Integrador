123934a7e6c8fe08b7dcaefd22564a05173c7d50
byjulie0 <julievieiramartins0@gmail.com>
2025-10-02 16:45:33 -0400

    Revert "Ajustando pop us"
    This reverts commit c5ee8980df19cd17693c28fa836a5ee692d17670.

--- Controller/overlays/pop_up_sucesso.php
+++ Controller/overlays/pop_up_sucesso.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pop Up Sucesso</title>
-    <link rel="stylesheet" href="./../../view/public/css/adm/pop_up.css">
-    <link rel="stylesheet" href="./../../view/js/pop_up.js">
+    <link rel="stylesheet" href="./../../view/public/css/adm/pop_up_sucesso.css">
     <script defer src="../../view/js/adm/pop_up_sucesso.js"></script>
 </head>
 <body>
