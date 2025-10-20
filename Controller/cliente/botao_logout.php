7301a0bbc95b9e8010e7676dd9e6134070d1d529
Eridan <genealerooo@gmail.com>
2025-10-20 16:08:39 -0400

    Adição de botões

---
+++ Controller/cliente/botao_logout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Componente do botao Logout</title>
+    <link rel="stylesheet" href="../../View/Public/css/cliente/botoes_cliente.css">
+    <script src="../../view/js/cliente/botao_logout.js"></script>
+</head>
+<body>
+    <button class="generate_report_product_button logout-confirm">
+        Logout
+    </button>
+</body>
+</html>
+
