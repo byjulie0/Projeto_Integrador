cb28e100166e6d338e44b9d92e8c3ec539d2f4c5
viny <vinyciusmarques43@gmail.com>
2025-10-22 15:17:59 -0400

    Squashed commit of the following:
    commit 2fcccc8ec36a1f3361af2740380062d3ef3b5eea
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 17:15:07 2025 -0400
    
        Remoção lixo na pagina
    
    commit 5c403ee463f174b8fb7273eeab2a33c773d0a438
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 17:11:42 2025 -0400
    
        Adicionada alteração de senha cliente
    
    commit 4df5bf4dba1ed1b99c73bd86f8a8b2dabda5b219
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:42:32 2025 -0400
    
        Update adm
    
    commit f7e4bea264607fe490329f55d078d4d51db88a5d
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:40:17 2025 -0400
    
        Catalogo de produtos
    
        Para exibir produtos
    
    commit 3dfc9b6daab2eb5f52c9d292fc68ebf9ef258273
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:15:14 2025 -0400
    
        Css botoes
    
    commit 97be1d4ad406c1c387dd1401ae96e956c601029e
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:13:10 2025 -0400
    
        Exclusão css botoes
    
    commit 327cbf8aa2f4a3a3fe2c08bfa649ee0da8e80f83
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:10:09 2025 -0400
    
        Adição pastas de pop_up e captcha
    
    commit 7301a0bbc95b9e8010e7676dd9e6134070d1d529
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:08:39 2025 -0400
    
        Adição de botões
    
    commit 9ac064f513a30eee3f4040b5aeaeedba9ce8f656
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:04:13 2025 -0400
    
        Squashed commit of the following:
    
        commit 4172e0b61aebfe89d2456b8f4ede9c52e2e95189
        Author: mariaaa <toledomiranda.maria23@gmail.com>
        Date:   Mon Oct 20 15:20:02 2025 -0400
    
            responsividade do meu perfil adm
    
        commit d14aad1d31a15cc17a343dbac0d135145c4fedca
        Author: mariaaa <toledomiranda.maria23@gmail.com>
        Date:   Mon Oct 20 14:35:24 2025 -0400
    
            resposividade verificar pedido
    
        commit 3d4e689bac59336a1abe03767943b87bcb7db21e
        Author: byjulie0 <julievieiramartins0@gmail.com>
        Date:   Fri Oct 17 17:08:13 2025 -0400
    
            Trocando ordem de categorias
    
    commit d3f0c12f9526e983aeb2f3c1e9a095fba2f010e1
    Author: Eridan <genealerooo@gmail.com>
    Date:   Mon Oct 20 16:03:44 2025 -0400
    
        Exclusão botões e arquivos excluidos
    
    commit f74c69511d962224b73ee3471ba884739c892b24
    Merge: cd885a5 5f59f9e
    Author: viny <vinyciusmarques43@gmail.com>
    Date:   Mon Oct 20 15:26:19 2025 -0400
    
        Merge pull request #209 from byjulie0/pops-de-adicinar/editar_produto
    
        Pops de adicinar/editar produto

--- Controller/cliente/botao_vermelho_cliente.php
+++ Controller/cliente/botao_vermelho_cliente.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Componente do botao Cancelar</title>
-    <link rel="stylesheet" href="../../View/Public/css/cliente/botao_cancelar.css">
+    <title>Componente do Botão Vermelho Cliente</title>
+    <link rel="stylesheet" href="../../View/Public/css/cliente/botoes_cliente.css">
     <script src="../../view/js/cliente/botao_cancelar.js"></script>
 </head>
 <body>
-    <button type="button" class="generate_report_product_button_cancelar-operacao">
-        Cancelar
+    <button type="button" class="generate_report_product_button botao_vermelho_cliente">
+        <?= $texto ?>
     </button>
 </body>
 </html>
