2d34133417e5fd47ba1e8cfbfbabafc71815dc76
byjulie0 <julievieiramartins0@gmail.com>
2025-10-24 17:13:05 -0400

    realizando modificações

--- Controller/cliente/recuperar_senha_login1.php
+++ Controller/cliente/recuperar_senha_login1.php
@@ -17,20 +17,28 @@ include 'menu_recuperar_senha.php';
 
     <main class="esqueci_senha_card_email_main">
         <section class="esqueci_senha_card_email_section">
-            <a href="#" onclick="window.history.back(); return false;" class="esqueci_senha_card_email_seta_voltar">
-                <i class="fa-solid fa-chevron-left"></i>
-            </a>
+           
             <div class="esqueci_senha_card_email">
                 <div class="esqueci_senha_card_email_content">
                     <h1>Esqueceu a Senha</h1>
-                    <h3>Digite seu e-mail para enviarmos o código e gerar uma nova senha</h3>
+                    <p>Digite seu e-mail para enviarmos o código e gerar uma nova senha</p>
                     <form action="recuperar_senha_login2.php" class="esqueci_senha_card_email_formulario" method="GET">
                         <input type="email" name="esqueci_senha_card_email_digitar" placeholder="E-mail" required>
-                        <?php
-                        $texto = "Enviar"; 
-                        include 'botao_cliente.php';
-                        ?>
                     </form>
+                    <div class="div_botao">
+                        <button>
+                            <?php
+                            $texto = "Enviar"; 
+                            include 'botao_verde_cliente.php';
+                            ?>
+                        </button>
+                        <button>
+                            <?php
+                            $texto = "Cancelar"; 
+                            include 'botao_vermelho_cliente.php';
+                            ?>
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>
