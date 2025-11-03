151cd1bdeee19d4786337405e45fd076f4fd6a6e
Gabrielbenial <gabrielbenial543@gmail.com>
2025-11-03 14:53:12 -0400

    Origin/gerar enviar senha GABRIEL (#217)
    * php mailer
    
    * gerar e enviar senha cliente
    
    * gerer_enviar_senha
    
    * Update gerar_enviar_senha.php
    
    * criptografia da senha
    
    ---------
    
    Co-authored-by: Lara Eridan <genealerooo@gmail.com>

--- Controller/cliente/recuperar_senha_login1.php
+++ Controller/cliente/recuperar_senha_login1.php
@@ -17,32 +17,25 @@ include 'menu_recuperar_senha.php';
 
     <main class="esqueci_senha_card_email_main">
         <section class="esqueci_senha_card_email_section">
-
-            <div class="esqueci_senha_card_email_content">
-                <h1>Esqueceu a Senha</h1>
-                <p>Digite seu e-mail para enviarmos o código e gerar uma nova senha</p>
-                <form action="../utils/gerar_enviar_senha.php" class="esqueci_senha_card_email_formulario"
-                    method="POST">
-                    <input type="email" name="esqueci_senha_card_email_digitar" placeholder="E-mail" required>
-                    <div class="botoes_div">
-                        <?php
-                        $texto = "Enviar";
-                        include 'botao_cliente.php';
-                        ?>
-                </form>
-                <div class="div_botao">
-                    <button>
-                        <?php
-                        $texto = "Enviar";
-                        include 'botao_verde_cliente.php';
-                        ?>
-                    </button>
-                    <button>
-                        <?php
-                        $texto = "Cancelar";
-                        include 'botao_vermelho_cliente.php';
-                        ?>
-                    </button>
+            <div class="esqueci_senha_card_email">
+                <div class="esqueci_senha_card_email_content">
+                    <h1>Esqueceu a Senha</h1>
+                    <p>Digite seu e-mail para enviarmos o código e gerar uma nova senha</p>
+                    <form action="../utils/gerar_enviar_senha.php" class="esqueci_senha_card_email_formulario" method="POST">
+                        <input type="email" name="esqueci_senha_card_email_digitar" placeholder="E-mail" required>
+                        <div class="botoes_div">
+                            <?php
+                            $texto = "Enviar"; 
+                            include 'botao_verde_cliente.php';
+                            ?>
+                            <a href="#" onclick="window.history.back(); return false;">
+                            <?php
+                            $texto = "Cancelar"; 
+                            include 'botao_vermelho_cliente.php';
+                            ?>
+                            </a>
+                        </div>
+                    </form>
                 </div>
             </div>
             </div>
