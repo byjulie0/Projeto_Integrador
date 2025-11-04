ed9fd102cf74a16c4f4776c5279c837459e9b9b9
byjulie0 <julievieiramartins0@gmail.com>
2025-11-04 15:03:18 -0400

    Update login.php

--- Controller/cliente/login.php
+++ Controller/cliente/login.php
@@ -8,7 +8,7 @@ include '../utils/libras.php'
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login - John Rooster</title>
+  <title>Login - John Rooster </title>
   <link rel="stylesheet" href="../../view/public/css/cliente/pg_login.css">
   <script src="../../view/js/cliente/login_olho_ocultar_senha.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
