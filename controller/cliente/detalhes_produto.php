c236b909c22175c56a536d60fd5518578a3f4e04
byjulie0 <julievieiramartins0@gmail.com>
2025-11-06 16:55:54 -0400

    FIX:  ajustando o front

--- controller/cliente/detalhes_produto.php
+++ controller/cliente/detalhes_produto.php
@@ -19,7 +19,6 @@ include 'menu_pg_inicial.php';
     <!-- <script>var usuarioLogado = <php echo isset($_SESSION['id_cliente']) ? 'true' : 'false'; ?>;</script> -->
     <h2 class="titulo-produto-detalhes-produto">
         <a href="#" onclick="window.history.back(); return false"><i class="bi bi-chevron-left"></i></a>
-        <?php echo htmlspecialchars($produto['prod_nome']); ?>
     </h2>
     <main class="main-detalhes-produto">
 
@@ -36,41 +35,48 @@ include 'menu_pg_inicial.php';
                 <img src="../../view/public/imagens/default-thumbnail.jpg" alt="Imagem Principal">
                 <!-- <img src="<php echo $produto['path_img']; ?>"
                     alt="Imagem Principal de <php echo htmlspecialchars($produto['prod_nome']); ?>"> -->
+                <h3 class="informacao">Informações</h3>
+                <p><?php echo $produto['descricao'] ? htmlspecialchars($produto['descricao']) : 'Descrição não disponível.'; ?>
+                </p>
             </div>
+            
         </div>
 
         <div class="info-produto-detalhes-produto">
 
-            <p class="informacoes-detalhes-produto">Vendido pela empresa <span>John Rooster</span></p>
-
-            <p class="informacoes-detalhes-produto">Entregue por <span>John Rooster</span></p>
-
-            <p class="informacoes-detalhes-produto">A John Rooster se compromete a oferecer apenas os melhores animais e
-                itens do mercado.</p>
-
-            <p class="preco-detalhes-produto">R$ <?php echo $valor_formatado; ?></p>
-
-            <form id="formCarrinho" action="add_carrinho.php" method="GET">
-                <!-- <input type="hidden" name="id_produto" value="<php echo $produto['id_produto']; ?>"> -->
-                <a type="button" class="botao-carrinho-detalhes-produto" href="../utils/add_carrinho.php?id_produto=<?php echo $produto['id_produto']; ?>">Adicionar ao carrinho</a>
-            </form>
-
             <section class="descricao-detalhes-produto">
-                <h3>Informações</h3>
-                <p><?php echo $produto['descricao'] ? htmlspecialchars($produto['descricao']) : 'Descrição não disponível.'; ?>
-                </p>
-            </section>
-            
-            <section class="sub-descricao-detalhes-produto">
-                <?php if ($produto['id_categoria'] != 5): ?>
+                <h2><?php echo htmlspecialchars($produto['prod_nome']);?></h2>
+
+                <section class="sub-descricao-detalhes-produto">
+                    <?php if ($produto['id_categoria'] != 5): ?>
                     <p><strong>Peso: </strong><?php echo $peso_formatado; ?></p>
                     <p><strong>Data de nascimento: </strong><?php echo $produto['idade']; ?></p>
                     <p><strong>Tipo: </strong><?php echo $produto['subcat_nome'] ?? 'Não categorizado'; ?></span></p>
                     <?php if ($produto['campeao']): ?>
-                        <p><strong>Status:</strong> <span class="badge bg-success">Animal Campeão</span></p>
+                            <p><strong>Status:</strong> <span class="badge bg-success">Animal Campeão</span></p>
                     <?php endif; ?>
-                <?php endif; ?>
+                    <?php endif; ?>
+                </section>
+                <p class="preco-detalhes-produto">R$ <?php echo $valor_formatado; ?></p>
+                <form id="formCarrinho" action="add_carrinho.php" method="GET">
+                    <!-- <input type="hidden" name="id_produto" value="<php echo $produto['id_produto']; ?>"> -->
+                    <a type="button" class="botao-carrinho-detalhes-produto" href="../utils/add_carrinho.php?id_produto=<?php echo $produto['id_produto']; ?>">Adicionar ao carrinho</a>
+                </form>
+                <div class="div_info_prod">
+                    <p class="informacoes-detalhes-produto">Vendido pela empresa <span>John Rooster</span></p>
+
+                    <p class="informacoes-detalhes-produto">Entregue por <span>John Rooster</span></p>
+
+                    <p class="informacoes-detalhes-produto">A John Rooster se compromete a oferecer apenas os melhores animais e
+                    itens do mercado.</p>
+                </div>
             </section>
+
+            
+
+           
+
+            
     </main>
 </body>
 <!-- <php include '../overlays/pop_up_login.php'; ?> -->
