68ad9faba67b51ba82e684e0f7e50afd9e05c6a8
Gabrielbenial <gabrielbenial543@gmail.com>
2025-11-13 15:59:44 -0400

    Update gerar_notificacao.php

--- Controller/utils/gerar_notificacao.php
+++ Controller/utils/gerar_notificacao.php
@@ -21,7 +21,29 @@ if ($stmt->execute()) {
 }
 
 
+}
 
 
- 
+
+function Criar_notificacao_adm($con, $adm_id, $produto_id, $mensagem, $categoria){
+
+
+$sql = "INSERT INTO notificacoes_adm (adm_id, produto_id, mensagem, categoria) VALUES (?, ?, ?, ?)";
+$stmt = $con->prepare($sql); 
+
+if (!$stmt) {
+    die("Erro ao preparar a query: " . $con->error);
+}
+
+
+$stmt->bind_param("iisss", $adm_id, $produto_id, $mensagem, $categoria);
+
+if ($stmt->execute()) {
+    return true; // sucesso
+} else {
+    error_log("Erro ao inserir notificação: " . $stmt->error);
+    return false; // falhou
+}
+
+
 }
