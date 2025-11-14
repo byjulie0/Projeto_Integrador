86c289f683de6046e0ba8fdc18298fd10ae4b756
byjulie0 <julievieiramartins0@gmail.com>
2025-11-14 16:03:14 -0400

    FIX: adicionando função de voltar

--- Controller/adm/meu_perfil_editar.php
+++ Controller/adm/meu_perfil_editar.php
@@ -30,7 +30,7 @@ include 'menu_inicial.php';
             <form class="client-edit-form" method="POST" action="../utils/atualizar_adm.php">
             <form class="client-edit-form">
                 <div class="client-edit-header">
-                    <h1 class="client-edit-title"><i class="bi bi-chevron-left"></i>Editar meus dados</h1>
+                    <h1 class="client-edit-title"><a href="#" onclick="window.history.back(); return false;"><i class="bi bi-chevron-left"></a></i>Editar meus dados</h1>
                 </div>
 
                 <div class="client-edit-grid">
