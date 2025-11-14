3442b353b42487eb943da6ad5f80428c48f9857b
MariArantes4 <arantesmariana839@gmail.com>
2025-11-14 16:54:38 -0400

    inicio task

--- Controller/cliente/card_telas.php
+++ Controller/cliente/card_telas.php
@@ -8,17 +8,42 @@
 </head>
 <?php
 $id_prod = $row['id_produto'];
-$imagem = $row['path_img'];
 $nome = $row['prod_nome'];
 $valor = number_format($row['valor'], 2, ',', '.');
+
+// PROCESSAR PATH_IMG - PEGAR APENAS A PRIMEIRA IMAGEM
+$imagemPrincipal = 'imagens/default-thumbnail.jpg'; // Imagem padrão
+
+if (!empty($row['path_img'])) {
+    $path = trim($row['path_img']);
+    
+    // Se for JSON (array de imagens)
+    if ($path[0] === '[') {
+        $listaImagens = json_decode($path, true);
+        if (!empty($listaImagens[0])) {
+            $imagemPrincipal = trim(str_replace('\\', '', $listaImagens[0]));
+        }
+    } 
+    // Se for string com vírgulas
+    else if (strpos($path, ',') !== false) {
+        $listaImagens = explode(',', $path);
+        $imagemPrincipal = trim(str_replace('\\', '', $listaImagens[0]));
+    }
+    // Se for imagem única
+    else {
+        $imagemPrincipal = trim(str_replace('\\', '', $path));
+    }
+}
 ?>
 <body>
     <div class="lote-card">
         <a href="detalhes_produto.php?id_produto=<?php echo $id_prod; ?>">
-            <img src="../../View/Public/<?php echo $imagem; ?>" alt="<?php echo $nome; ?>">
+            <img src="../../View/Public/<?php echo htmlspecialchars($imagemPrincipal); ?>" 
+                 alt="<?php echo htmlspecialchars($nome); ?>"
+                 onerror="this.src='../../View/Public/imagens/default-thumbnail.jpg'">
             <div class="info-grid">
                 <p>Nome:</p>
-                <p><?php echo $nome; ?></p>
+                <p><?php echo htmlspecialchars($nome); ?></p>
                 <p class="preco">R$ <?php echo $valor; ?></p>
             </div>
         </a>
