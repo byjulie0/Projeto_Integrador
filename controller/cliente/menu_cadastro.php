ce202d41296ed7af3ee8c499e21c1f8b76f3ff4b
byjulie0 <julievieiramartins0@gmail.com>
2025-11-28 17:06:49 -0400

    FIX: modificando caminho

--- controller/cliente/menu_cadastro.php
+++ controller/cliente/menu_cadastro.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Cadastro</title>
-    <link rel="stylesheet" href="/Projeto_Integrador/view/public/css/cliente/menu_geral.css">
+    <link rel="stylesheet" href="../../view/public/css/cliente/menu_geral.css">
 </head>
 <body>
     <nav class="menu_login">
         <div class="div_menu_login">
             <div class="logo_menu_login">
-                <img src="../../View/Public/Imagens/img_logofabrica.png" alt="Logo John Rooster">
+                <img src="../../view/public/imagens/img_logofabrica.png" alt="Logo John Rooster">
             </div>
             <h2 class="titulo_menu_login">John Rooster - Cadastro</h2>
         </div>
